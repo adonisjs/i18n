20d9c8955513afb88cd83c8c850aef6c0c9571ce
Lupiac <antoine.lupiac0@gmail.com>
2025-07-16 10:21:35 +0200

    fix: add support for vinejs messages provider by getting otherField (#141)

--- src/vine_i18n_messages_provider.ts
+++ src/vine_i18n_messages_provider.ts
@@ -51,6 +51,17 @@ export class I18nMessagesProvider implements MessagesProviderContact {
     if (translatedFieldName) {
       fieldName = this.#i18n.formatRawMessage(translatedFieldName.message)
     }
+    /**
+     * Checking if vine's meta comes with otherField
+     */
+    if (meta?.otherField) {
+      const translatedOtherFieldName = this.#i18n.resolveIdentifier(
+        `${this.#fieldsPrefix}.${meta.otherField}`
+      )
+      if (translatedOtherFieldName) {
+        meta.otherField = this.#i18n.formatRawMessage(translatedOtherFieldName.message)
+      }
+    }
 
     /**
      * 1st priority is given to the field messages
@@ -64,7 +75,6 @@ export class I18nMessagesProvider implements MessagesProviderContact {
         ...meta,
       })
     }
-
     /**
      * 2nd priority is for rule messages
      */
